2b20fc1b79719f67ed3aeb632f0cd539cb907bb4
Luis Reyes <reyesrosadoluis1@gmail.com>
2021-12-03 22:00:35 -0500

    Main menu button on Result Page works as desired

--- js/gui.js
+++ js/gui.js
@@ -103,10 +103,24 @@ function closeMainMenu() {
     i.style.visibility = "hidden";
   }
   showMenu = false;
+  showGui = false;
   document.getElementById("crosshair").src =
     repo + "assets/Textures/crosshairs/" + GUI.crosshair + ".png";
   showCrosshair();
   canvas.requestPointerLock();
+}
+
+function closeResultMenu() {
+  console.log("closing result screen");
+  const list2 = document.querySelectorAll(".result");
+  for (let i of list2) {
+    i.style.visibility = "hidden";
+  }
+  showMenu = false;
+  document.getElementById("crosshair").src =
+    repo + "assets/Textures/crosshairs/" + GUI.crosshair + ".png";
+  showCrosshair();
+  document.exitPointerLock();
 }
 
 function openMenu() {
@@ -117,6 +131,7 @@ function openMenu() {
     i.style.visibility = "visible";
   }
   showMenu = true;
+  showGui = false;
   document.getElementById("crosshair").src = "";
 }
 
